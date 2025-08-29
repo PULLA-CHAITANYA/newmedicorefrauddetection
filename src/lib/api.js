98e134296ecc9efb06bea7905e6514de8f5b97ec
PULLA-CHAITANYA 111622201083 <119579091+PULLA-CHAITANYA@users.noreply.github.com>
2025-08-29 21:39:27 +0530

    Update api.js

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: "https://medicarefraudservice-fahvamaaftc5e9f2.centralindia-01.azurewebsites.net/api",
 });
 
 api.interceptors.request.use((cfg) => {
